8ee7a354becc0ac42d584454adcb8b7f5fba804b
Dayanand maurya <dayanandmaurya405@gmail.com>
2026-06-26 11:55:58 +0530

    update api

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("first file");
+const express = require("express")
+
+const app = express();
+
+let students = [
+    {id: 1, name: "Dayanand", city: "gorkhapur"},
+    {id: 2, name: "Sachin", city: "gkp"}
+];
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("API IS RUNNING");
+});
+
+app.get("/students",(req,res)=>{
+    res.json({
+        message:"ALL students",
+        data: students
+    });
+});
+
+app.post("/students",(req,res)=>{
+    const{ id, name, city} = req.body;
+    const newStudent = { id,name, city};
+    students.push(newStudent);
+    res.json({
+        message:"Record Added",
+        student:newStudent,
+        data:students
+    })
+});
+
+
+app.listen(3000,()=>{
+    console.log("server started");
+})
+
